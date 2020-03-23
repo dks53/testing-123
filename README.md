4579332e29cda2c4985a0f75f984b4a12756279a
Darshil Shah <dks53@georgetown.edu>
2020-03-24 03:24:12 +0530

    recognize repo

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # testing-123
+
+## Setup
+
+
+
+## Usage
+
+Run the script:
+
+```sh
+```
+
+## Testing
+
+Run the test:
+
+```sh
+pytest
+```
---
+++ conftest.py
@@ -0,0 +1,5 @@
+# nothing to see here
+
+# helping pytest recognize the structure of the repo
+
+# put this in the root directory of the repository
